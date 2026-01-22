5292afdf700492b75b5adfa8c1a846e2ccad1ca5
minamoto64 <254497286+minamoto64@users.noreply.github.com>
2026-01-22 23:57:50 +0900

    feat: add name and admin attributes to users

--- db/migrate/20260122140758_create_users.rb
+++ db/migrate/20260122140758_create_users.rb
@@ -1,8 +1,15 @@
 class CreateUsers < ActiveRecord::Migration[8.1]
   def change
     create_table :users do |t|
+      # --- authentication ---
       t.string :email_address, null: false
       t.string :password_digest, null: false
+
+      # --- profile ---
+      t.string :name, null: false
+
+      # --- authorization ---
+      t.boolean :admin, default: false, null: false
 
       t.timestamps
     end
