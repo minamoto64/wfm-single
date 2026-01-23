f466fbdd9851601714f1f71b089c6e4a28244abb
minamoto64 <254497286+minamoto64@users.noreply.github.com>
2026-01-24 00:04:11 +0900

    chore: add initial admin and user seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,25 @@
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+# 管理者ユーザー
+admin = User.create!(
+  name: "管理者",
+  email_address: "admin@example.com",
+  password: "password",
+  password_confirmation: "password",
+  admin: true
+)
+
+#一般ユーザー
+user = User.create!(
+  name: "山田太郎",
+  email_address: "yamada@example.com",
+  password: "password",
+  password_confirmation: "password",
+  admin: false
+)
+
+puts "初期ユーザーを作成しました！"
+puts "管理者: #{admin.email_address} / password"
+puts "従業員: #{user.email_address} / password"
