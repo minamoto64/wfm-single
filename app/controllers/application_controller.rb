10dfa7937fcf24a497ae05dc5a5f026fa401638e
minamoto64 <254497286+minamoto64@users.noreply.github.com>
2026-01-23 20:12:34 +0900

    feat: add admin authorization check to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,12 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  private
+
+  def require_admin
+    unless current_user&.admin?
+      redirect_to root_path, alert: "管理者権限が必要です"
+    end
+  end
 end
