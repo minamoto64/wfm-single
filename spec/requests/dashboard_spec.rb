7323cb10347147522ac3b3cebad8cc251ad7df8c
minamoto64 <254497286+minamoto64@users.noreply.github.com>
2026-01-24 22:27:55 +0900

    test: add dashboard request specs

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -1,11 +1,35 @@
 require 'rails_helper'
 
-RSpec.describe "Dashboards", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/dashboard/index"
-      expect(response).to have_http_status(:success)
+RSpec.describe 'Dashboards', type: :request do
+  let(:admin_user) { create(:user, name: 'Admin User', admin: true) }
+  let(:normal_user) { create(:user, name: 'Normal User', admin: false) }
+
+  before do
+    post session_path, params: { email_address: login_user.email_address, password: login_user.password }
+    get root_path
+  end
+
+  context 'as admin user' do
+    let(:login_user) { admin_user }
+
+    it 'shows user name' do
+      expect(response.body).to include('こんにちは、Admin User')
+    end
+
+    it 'shows admin label' do
+      expect(response.body).to include('管理者')
     end
   end
 
+  context 'as normal user' do
+    let(:login_user) { normal_user }
+
+    it 'shows user name' do
+      expect(response.body).to include('こんにちは、Normal User')
+    end
+
+    it 'does not show admin label' do
+      expect(response.body).not_to include('管理者')
+    end
+  end
 end
