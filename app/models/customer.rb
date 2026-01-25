6406dc3875fcc1dde15aa98397acdc7f4afbab75
minamoto64 <254497286+minamoto64@users.noreply.github.com>
2026-01-25 10:27:42 +0900

    feat: add validations to Customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,9 @@
 class Customer < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, allow_blank: true
+  validates :phone, format: { with: /\A0\d{1,4}-\d{1,4}-\d{4}\z/ }, allow_blank: true
+  validates :key_notes, length: { maximum: 500 }
+
+  # add associations after other models are created
+  # has_many :interactions
 end
