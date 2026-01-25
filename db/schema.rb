c0a206fe4c6fe000d1f74d75798b1e11e344b74f
minamoto64 <254497286+minamoto64@users.noreply.github.com>
2026-01-25 09:33:49 +0900

    feat: create customers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_22_140759) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_25_001400) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "customers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.text "key_notes"
+    t.string "name", null: false
+    t.string "phone"
+    t.datetime "updated_at", null: false
+  end
 
   create_table "sessions", force: :cascade do |t|
     t.datetime "created_at", null: false
---
+++ spec/factories/customers.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :customer do
+    name { "MyString" }
+    phone { "MyString" }
+    key_notes { "MyText" }
+  end
+end
