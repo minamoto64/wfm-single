e451fac9e9cc94a6d76877d79048ff4d4c875a2e
minamoto64 <254497286+minamoto64@users.noreply.github.com>
2026-05-10 22:42:54 +0900

    feat:implement customers index with request spec

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,23 @@
 class CustomersController < ApplicationController
+  before_action :set_customer, only: [ :show, :edit, :update ]
+
+  def index
+    @customers = Customer.order(:name)
+  end
+
+  def new; end
+  def create; end
+  def show; end
+  def edit; end
+  def update; end
+
+  private
+
+  def set_customer
+    @customer = Customer.find(params[:id])
+  end
+
+  def customer_params
+    params.require(:customer).permit(:name, :email, :phone, :key_notes)
+  end
 end
--- spec/requests/customers_spec.rb
+++ spec/requests/customers_spec.rb
@@ -1,7 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe "Customers", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+
+  def sign_in(user)
+    post session_path, params: { email_address: user.email_address, password: "password55" }
+  end
+
+  describe "GET /customers" do
+    context "when the user is logged in" do
+      before { sign_in(user) }
+
+      it "responds with HTTP 200 OK" do
+        get customers_path
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "displays the customers list" do
+        customer = create(:customer)
+        get customers_path
+        expect(response.body).to include(customer.name)
+        expect(response.body).to include(customer.email)
+        expect(response.body).to include(customer.phone)
+        expect(response.body).to include(customer.key_notes)
+      end
+    end
+
+    context "when the user is not logged in" do
+      it "redirects to the login page" do
+        get customers_path
+        expect(response).to redirect_to(new_session_path)
+      end
+    end
   end
 end
