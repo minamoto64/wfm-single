05a31c28cc3599ac47a8e0f3df2bf2b341684448
minamoto64 <254497286+minamoto64@users.noreply.github.com>
2026-05-23 13:14:09 +0900

    feat: add root association to Notice

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -1,8 +1,14 @@
 class Notice < ApplicationRecord
+  before_validation :assign_root
+  after_create :assign_self_as_root
+
   belongs_to :user
 
   belongs_to :parent, class_name: "Notice", optional: true
   has_many :children, class_name: "Notice", foreign_key: "parent_id"
+
+  belongs_to :root, class_name: "Notice", optional: true
+  has_many :thread_notices, class_name: "Notice", foreign_key: :root_id, dependent: :nullify
 
   # add associations after other models are created
   # has_many :interactions
@@ -20,4 +26,16 @@ class Notice < ApplicationRecord
   validates :start_at, presence: true
   validates :end_at, presence: true
   validates :end_at, comparison: { greater_than: :start_at }, if: -> { start_at.present? && end_at.present? }
+
+  private
+
+  def assign_root
+    return unless parent
+
+    self.root = parent.root || parent
+  end
+
+  def assign_self_as_root
+    update_column(:root_id, id) if root_id.nil?
+  end
 end
