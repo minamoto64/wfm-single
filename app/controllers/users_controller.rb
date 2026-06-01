1ac12d14aeb6ed4a2fdc14b44269d1594a12a508
minamoto64 <254497286+minamoto64@users.noreply.github.com>
2026-06-01 22:51:15 +0900

    feat:implement users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update]
+  before_action :require_admin, only: %i[new create edit update]
+
+  def index
+    @users = User.order(:name)
+  end
+
+  def new; end
+  def create; end
+  def show; end
+  def edit; end
+  def update; end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    permitted = %i[
+      name
+      email_address
+      admin
+    ]
+
+    permitted << :password if params.require(:user)[:password].present?
+
+    params.require(:user).permit(permitted)
+  end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let!(:admin) { create(:user, admin: true) }
+
+  def sign_in(user)
+    post session_path, params: { email_address: user.email_address, password: "password55" }
+  end
+
+  describe "GET /users" do
+    context "when the user is logged in" do
+      before { sign_in(user) }
+
+      it "responds with HTTP 200 OK" do
+        get users_path
+
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "displays the users list" do
+        get users_path
+
+        expect(response.body).to include(user.name)
+        expect(response.body).to include(admin.name)
+        expect(response.body).to include(I18n.t("users.admin.#{user.admin?}"))
+      end
+    end
+
+    context "when the user is not logged in" do
+      it "redirects to the login page" do
+        get users_path
+
+        expect(response).to redirect_to new_session_path
+      end
+    end
   end
 end
