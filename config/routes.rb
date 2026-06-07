219a5e61b6c8d053dc68de016db1b517f58b4916
minamoto64 <254497286+minamoto64@users.noreply.github.com>
2026-06-07 15:38:39 +0900

    refactor: replace dashboard landing page with interactions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "dashboard#index"
+  root "interactions#index"
   resource :session
   resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
