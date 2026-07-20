8c457af9c9a2a2d2e76aefb672c2a000220e1fc1
minamoto64 <254497286+minamoto64@users.noreply.github.com>
2026-07-21 01:16:36 +0900

    test(comments): add coverage for restricted-record comment authorization
    Cover non-admin rejection and admin bypass for restricted task/notice comments.

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Comments", type: :request do
   let(:user) { create(:user) }
-  let(:other_user) { create(:other_user) }
+  let(:other_user) { create(:user) }
 
   def sign_in(user)
     post login_path, params: { email_address: user.email_address, password: "password55" }
@@ -36,9 +36,42 @@ RSpec.describe "Comments", type: :request do
     end
   end
 
+  shared_examples "restricts posting comments" do |commentable_factory|
+    let(:commentable) { create(commentable_factory, user: other_user, restricted: true) }
+
+    it "does not allow a non-admin user to comment on a restricted record" do
+      expect {
+        post comments_path, params: {
+          commentable_type: commentable.class.name,
+          commentable_id: commentable.id,
+          comment: { content: "テストコメント" }
+        }
+      }.not_to change(Comment, :count)
+
+      expect(response).to have_http_status(:not_found)
+    end
+
+    it "allows an admin user to comment on a restricted record" do
+      sign_in(create(:user, admin: true))
+
+      expect {
+        post comments_path, params: {
+          commentable_type: commentable.class.name,
+          commentable_id: commentable.id,
+          comment: { content: "テストコメント" }
+        }
+      }.to change(Comment, :count).by(1)
+
+      expect(response).to redirect_to commentable
+    end
+  end
+
   describe "POST /comments" do
     it_behaves_like "allows posting comments", "interaction"
     it_behaves_like "allows posting comments", "task"
     it_behaves_like "allows posting comments", "notice"
+
+    it_behaves_like "restricts posting comments", "task"
+    it_behaves_like "restricts posting comments", "notice"
   end
 end
